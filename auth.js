4d66a0e19f0a1a8863b8f635db46b1a99f3956d7
bcoghi <bcoghill601@gmail.com>
2024-12-04 16:13:11 -0500

    current auth

---
+++ auth.js
@@ -0,0 +1,42 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2dx3J_oBeEIDVXXsBytZeSew9G6GQC1s",
+    authDomain: "csci-final-583ca.firebaseapp.com",
+    projectId: "csci-final-583ca",
+    storageBucket: "csci-final-583ca.firebasestorage.app",
+    messagingSenderId: "283102873793",
+    appId: "1:283102873793:web:d3a20c07a30620135b2c40",
+    measurementId: "G-CHFDD3EMB9"
+};
+
+//Initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+//Login using popup
+const log = document.getElementById("login");
+log.submit(function (e) {
+    console.log("Submit clicked");
+    var provider = new firebase.auth.GoogleAuthProvider();
+        
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // IdP data available in result.additionalUserInfo.profile.
+    // ...
+    }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+});
